04932bfc24a51d2e90043688bde35a6c2eca49ad
West-Ben <benjamin.west.217@gmail.com>
2019-03-11 10:41:40 -0700

    bezier complete

--- main.cpp
+++ main.cpp
@@ -21,6 +21,42 @@ using namespace std;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
+std::vector<float> control_pointsX;
+std::vector<float> control_pointsY;
+
+float factorial (int n)
+{
+	if (n <= 1 ) return 1;
+	else return n * factorial(n - 1);
+}
+
+float combination (int n,int k)
+{
+	return ((factorial(n)) / (factorial(k) * factorial(n - k)));
+}
+
+float binomaial (int n, int k, float t)
+{
+	float result = 0;
+	
+	if (k == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			result += combination(n,i) * pow(t,i) * pow((1-t),(n-i)) * control_pointsX[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			result += combination(n,i) * pow(t,i) * pow((1-t),(n-i)) * control_pointsY[i];
+		}
+	}
+
+	
+	return result;
+}
 
 void GL_render()
 {
@@ -30,10 +66,28 @@ void GL_render()
     glBegin(GL_LINES);
     glColor3f(1.0f,0.0f,0.0f);
     // just for example, remove if desired
-    glVertex2f(-.5f,-.5f);
-    glVertex2f(.5f,-.5f);
-    glVertex2f(.5f,.5f);
-    glVertex2f(-.5f,.5f);
+	if (control_pointsX.size() != 0)
+	{
+		//glVertex2f(-.5f,-.5f);
+		//glVertex2f(.5f,-.5f);
+		float Bx = 0, By = 0;
+		vec2 F;
+		F[0] = 0;
+		F[1] = 0;
+		for(float i = 0 ; i <= 1 ; i += 0.00001)
+		{
+			Bx = binomaial(control_pointsX.size(),0,i);
+			By = binomaial(control_pointsY.size(),1,i);
+			for (int j = 0; j < control_pointsX.size(); j++)
+			{
+				F[0] += Bx * control_pointsX[j];
+				F[1] += By * control_pointsY[j]; 
+			}
+			glVertex2f(Bx, By);
+		}
+	}
+    //glVertex2f(.5f,.5f);
+    //glVertex2f(-.5f,.5f);
     glEnd();
     glFlush();
 }
@@ -52,7 +106,12 @@ void GL_mouse(int button,int state,int x,int y)
         double px,py,dummy_z; // we don't care about the z-value but need something to pass in
         gluUnProject(x,y,0,mv_mat,proj_mat,vp_mat,&px,&py,&dummy_z);
         glutPostRedisplay();
-    }
+		
+		control_pointsX.push_back(px);
+		control_pointsY.push_back(py);
+	}
+	
+	
 }
 
 //Initializes OpenGL attributes
@@ -64,7 +123,7 @@ void GLInit(int* argc, char** argv)
 
     //glMatrixMode(GL_PROJECTION_MATRIX);
     //glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, -1, 1);
-    glutCreateWindow("CS 130 - <Insert Name Here>");
+    glutCreateWindow("CS 130 - <Benjamin West>");
     glutDisplayFunc(GL_render);
     glutMouseFunc(GL_mouse);
 }
